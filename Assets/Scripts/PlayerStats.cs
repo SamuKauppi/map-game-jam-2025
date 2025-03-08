6c453574ea1244fbf8ca1a107f86ee42700153a0
patheticmaggot <miskajs190799@gmail.com>
2025-03-08 17:59:41 +0200

    Trying to make horse lose stamina while moving

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -12,6 +12,7 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] private int money = 100;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private bool weather = false;
+    [SerializeField] private float staminaDifficultyScale = 0.1f;
 
     public int Health => health;
     public int GameTime => time;
@@ -87,25 +88,6 @@ public class PlayerStats : MonoBehaviour
         UiManager.Instance.UpdateHealthUI(health);
     }
 
-    public void SetPlayerPos(Vector2 newLocation)
-    {
-        transform.position = newLocation;
-    }
-    public void PlayerMove(Transform[] moveRoute, RouteType type)
-    {
-        StartCoroutine(MoveBetweenPoints(moveRoute, type));
-    }
-
-    public void TakeTime(int TakenTime)
-    {
-        time -= TakenTime;
-        UiManager.Instance.UpdateTimeUI(time);
-        if (time <= 0)
-        {
-            GameOverManager.Instance.TriggerGameOver();
-        }
-    }
-
     public void HorseTired(int HorseStaminaMinus)
     {
         horseStamina -= HorseStaminaMinus;
@@ -122,6 +104,45 @@ public class PlayerStats : MonoBehaviour
         if (money <= 0)
         {
             GameOverManager.Instance.TriggerGameOver();
+        }
+    }
+
+    public void TakeTime(int TakenTime)
+    {
+        time -= TakenTime;
+        UiManager.Instance.UpdateTimeUI(time);
+        if (time <= 0)
+        {
+            GameOverManager.Instance.TriggerGameOver();
+        }
+    }
+
+    public void SetPlayerPos(Vector2 newLocation)
+    {
+        transform.position = newLocation;
+    }
+    public void PlayerMove(Transform[] moveRoute, RouteType type)
+    {
+        DecreaseStaminaWhileMoving(moveRoute[0], moveRoute[moveRoute.Length - 1], type);
+        StartCoroutine(MoveBetweenPoints(moveRoute, type));
+    }
+
+    private void DecreaseStaminaWhileMoving(Transform startPos, Transform endPos, RouteType type) 
+    { 
+        float distance = Vector2.Distance(startPos.position, endPos.position);
+        float decrease = distance * RouteMultiplier(type) * staminaDifficultyScale;
+        HorseTired(Mathf.RoundToInt(decrease));
+    }
+
+    private float RouteMultiplier(RouteType type)
+    {
+        switch (type)
+        {
+            case RouteType.Road: return 1f;
+            case RouteType.Offroad: return 2f;
+            case RouteType.Boat: return 0.75f;
+            case RouteType.Ship: return 0.5f;
+            default: return 1f;
         }
     }
 
