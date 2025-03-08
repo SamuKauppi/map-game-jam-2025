40c53b8ebe8f4e6a491198b208e914aca8b1479a
patheticmaggot <miskajs190799@gmail.com>
2025-03-08 14:46:35 +0200

    Added quest pointer

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     [SerializeField] private RoutePoint[] endPoints;
+    [SerializeField] private Transform questPointer;
 
     private RoutePoint currentTarget;
 
@@ -27,6 +28,7 @@ public class GameManager : MonoBehaviour
     public void ChooseNewDestination()
     {
         currentTarget = endPoints[Random.Range(0, endPoints.Length)];
+        questPointer.transform.position = currentTarget.transform.position;
     }
 
     public void ReachedDestination(RoutePoint destination)
